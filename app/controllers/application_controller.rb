9f8678197857e6a4df4eafb0c320b2507619177a
Travis Gaff <travis@labzero.com>
2024-03-21 14:11:59 +0900

    basic auth (primarily just because of the openAI credentials)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+  
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.dig(:basic_auth, :username) && password == Rails.application.credentials.dig(:basic_auth, :password)
+    end
+  end
 end
