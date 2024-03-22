fd4315e40f87849532167551ebc432fefafb97f0
Travis Gaff <travis@labzero.com>
2024-03-22 15:39:39 +0900

    you really don't need that key

--- config/initializers/langchainrb_rails.rb
+++ config/initializers/langchainrb_rails.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-LangchainrbRails.configure do |config|
-  config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
-    llm: Langchain::LLM::OpenAI.new(
-      api_key: Rails.application.credentials.dig(:openai_key),
-      # using text-embedding-3-small because the default ada-2 fails due to a bug in inappropriately sending dimensions
-      default_options: { embeddings_model_name: "text-embedding-3-small" }
+unless ENV['SECRET_KEY_BASE_DUMMY']
+    LangchainrbRails.configure do |config|
+    config.vectorsearch = Langchain::Vectorsearch::Pgvector.new(
+      llm: Langchain::LLM::OpenAI.new(
+        api_key: Rails.application.credentials.dig(:openai_key),
+        # using text-embedding-3-small because the default ada-2 fails due to a bug in inappropriately sending dimensions
+        default_options: { embeddings_model_name: "text-embedding-3-small" }
+      )
     )
-  )
+  end
 end
